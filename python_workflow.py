e3e7016a54c075742f71a6c04781857744e97d20
Phyrik <34688504+Phyrik@users.noreply.github.com>
2020-07-28 20:02:48 +0100

    moved python_workflow.py

--- python_workflow.py
+++ python_workflow.py
@@ -1,4 +1,4 @@
-with open('../../docs/index.html', 'w+') as output, open('../../README.html', 'r') as input:
+with open('./docs/index.html', 'w+') as output, open('README.html', 'r') as input:
     while True:
         data = input.read(100000)
         if data == '':  # end of file reached
