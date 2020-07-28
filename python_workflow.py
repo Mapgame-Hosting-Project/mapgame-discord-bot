83bbaa44079a19790faa3686684cca27d8112a0d
Phyrik <34688504+Phyrik@users.noreply.github.com>
2020-07-28 20:21:08 +0100

    Delete python_workflow.py

--- python_workflow.py
+++
@@ -1,6 +0,0 @@
-with open('./docs/index.html', 'w+') as output, open('README.html', 'r') as input:
-    while True:
-        data = input.read(100000)
-        if data == '':  # end of file reached
-            break
-        output.write(data)
