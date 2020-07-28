088e82b128a0b6e440916531e9e9ffaa29c1ba04
Phyrik <34688504+Phyrik@users.noreply.github.com>
2020-07-28 19:58:43 +0100

    Create python_workflow.py

---
+++ .github/workflows/python_workflow.py
@@ -0,0 +1,6 @@
+with open('../../docs/index.html', 'w+') as output, open('../../README.html', 'r') as input:
+    while True:
+        data = input.read(100000)
+        if data == '':  # end of file reached
+            break
+        output.write(data)
