70e86dfa70d68372bd06d2013e47b3bb28bbd3d3
Phyrik <34688504+Phyrik@users.noreply.github.com>
2020-07-30 13:55:00 +0100

    edited map on province picker 5

--- docs/js/map-province-picker.js
+++ docs/js/map-province-picker.js
@@ -9,7 +9,9 @@ var canvas = document.getElementById("main-canvas")
 var canvasCTX = canvas.getContext("2d")
 var img1 = new Image()
 img1.onload = function() {
-    canvasCTX.drawImage(img1, 0, 0, img1.width, img1.height)
+    canvas.width = img1.width
+    canvas.height = img1.height
+    canvasCTX.drawImage(img1, 0, 0)
 }
 img1.src = "res/edited map.png"
 canvas.addEventListener("click", function(e) {
