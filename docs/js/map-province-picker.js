c3759942e3260e93c4df8884055461ad3dd51a88
Phyrik <34688504+Phyrik@users.noreply.github.com>
2020-07-30 14:16:14 +0100

    edited map on province picker 10

--- docs/js/map-province-picker.js
+++ docs/js/map-province-picker.js
@@ -6,15 +6,18 @@ function handleMainCanvasClick(canvas, event) {
     alert(canvas.getContext("2d").getImageData(event.offsetX, event.offsetY, 1, 1).data)
 }
 
-var canvas = document.getElementById("main-canvas")
-var canvasCTX = canvas.getContext("2d")
-var img1 = new Image()
-img1.onload = function() {
-    canvas.width = img1.width
-    canvas.height = img1.height
-    canvasCTX.drawImage(img1, 0, 0)
-}
-img1.src = "res/edited map.png"
-canvas.addEventListener("click", function(e) {
-    handleMainCanvasClick(canvas, e)
+fetch("../res/colours with coords.json").then(response => response.json()).then(coloursWithCoordsJSON => {
+    alert(coloursWithCoordsJSON)
+    var canvas = document.getElementById("main-canvas")
+    var canvasCTX = canvas.getContext("2d")
+    var img1 = new Image()
+    img1.onload = function() {
+        canvas.width = img1.width
+        canvas.height = img1.height
+        canvasCTX.drawImage(img1, 0, 0)
+    }
+    img1.src = "res/edited map.png"
+    canvas.addEventListener("click", function(e) {
+        handleMainCanvasClick(canvas, e)
+    })
 })
