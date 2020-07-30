05ee6b41c7808043c23e19838a1b655d9dca81c9
Phyrik <34688504+Phyrik@users.noreply.github.com>
2020-07-30 13:46:54 +0100

    edited map on province picker

---
+++ docs/js/map-province-picker.js
@@ -0,0 +1,17 @@
+function handleMainCanvasClick(canvas, event) {
+    const rect = canvas.getBoundingClientRect()
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
+    alert("x: " + x + "y: " + y)
+}
+
+var canvas = document.getElementById("main-canvas")
+var canvasCTX = canvas.getContext("2d")
+var img1 = new Image()
+img1.onload = function() {
+    canvasCTX.drawImage(img1, 0, 0)
+}
+img1.src = "res/edited map.png"
+canvas.addEventListener("click", function(e) {
+    handleMainCanvasClick(canvas, e)
+})
